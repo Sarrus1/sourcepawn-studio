94fcd2fb80973dd91d84d89126b28459a6abf500
Sarrus1 <omegasix0611@gmail.com>
2021-03-03 22:01:25 +0100

    Added more icons and cleaned up the code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
+import { TextDocument } from "vscode-languageserver-textdocument";
 import {
-	TextDocument
-} from 'vscode-languageserver-textdocument';
-import { 
-    createConnection,
-    TextDocuments,
-    TextDocumentSyncKind,
-    ProposedFeatures,
-		DidChangeConfigurationNotification,
-		InitializeParams
+  createConnection,
+  TextDocuments,
+  TextDocumentSyncKind,
+  ProposedFeatures,
+  DidChangeConfigurationNotification,
+  InitializeParams,
 } from "vscode-languageserver/node";
 
-import { CompletionRepository } from './completions';
+import { CompletionRepository } from "./completions";
 
 //let sm_home: string = Workspace.getConfiguration("sourcepawnLanguageServer").get("sourcemod_home");
 let connection = createConnection(ProposedFeatures.all);
@@ -24,71 +22,70 @@ let workspaceRoot: string;
 let hasConfigurationCapability: boolean = false;
 
 connection.onInitialize((params) => {
-    workspaceRoot = params.workspaceFolders[0].uri;
-		let capabilities = params.capabilities;
-		hasConfigurationCapability = !!(
-			capabilities.workspace && !!capabilities.workspace.configuration
-		);
-    return {
-        capabilities: {
-            textDocumentSync: TextDocumentSyncKind.Full,
-            completionProvider: {
-                resolveProvider: false
-            },
-            signatureHelpProvider: {
-                triggerCharacters: ["("]
-            }
-        }
-    };
+  workspaceRoot = params.workspaceFolders[0].uri;
+  let capabilities = params.capabilities;
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  );
+  return {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Full,
+      completionProvider: {
+        resolveProvider: false,
+      },
+      signatureHelpProvider: {
+        triggerCharacters: ["("],
+      },
+    },
+  };
 });
 
 connection.onInitialized(async (params: InitializeParams) => {
-	if (hasConfigurationCapability) {
+  if (hasConfigurationCapability) {
     // Register for all configuration changes.
-    connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-	let sm_home = await f1();
-	init_parse_sm_api(sm_home);
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+  let sm_home = await GetSmHome();
+  init_parse_sm_api(sm_home);
 });
 
-async function f1() {
+async function GetSmHome() {
   let config = (await getDocumentSettings(workspaceRoot)).sourcemod_home;
-	return config;
-}
-
-function rejected(result) {
-  console.error(result);
+  return config;
 }
 
 async function init_parse_sm_api(sm_home) {
-	console.debug("DEBUG1", sm_home);
-	if (sm_home) {
-		console.debug("DEBUG2", sm_home);
-		completions.parse_sm_api(sm_home);
-	}
+  if (sm_home) {
+    completions.parse_sm_api(sm_home);
+  }
 }
 
 interface SourcepawnSettings {
-  sourcemod_home: string,
-	ServerOutput: string
+  sourcemod_home: string;
+  ServerOutput: string;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
-const defaultSettings: SourcepawnSettings = { sourcemod_home: "",
-																							ServerOutput: "off" };
+const defaultSettings: SourcepawnSettings = {
+  sourcemod_home: "",
+  ServerOutput: "off",
+};
 let globalSettings: SourcepawnSettings = defaultSettings;
 
 // Cache the settings of all open documents
 let documentSettings: Map<string, Thenable<SourcepawnSettings>> = new Map();
 
 connection.onDidChangeConfiguration((change) => {
-	let sm_home = change.settings.sourcepawnLanguageServer.sourcemod_home;
-	if(sm_home) {
-		completions.parse_sm_api(sm_home);
-	}
-})
+  let sm_home = change.settings.sourcepawnLanguageServer.sourcemod_home;
+  if (sm_home) {
+    completions.parse_sm_api(sm_home);
+  }
+});
 // Qu'est ce qu'on passe à la fonction en fait?
 function getDocumentSettings(resource: string): Thenable<SourcepawnSettings> {
   if (!hasConfigurationCapability) {
@@ -98,7 +95,7 @@ function getDocumentSettings(resource: string): Thenable<SourcepawnSettings> {
   if (!result) {
     result = connection.workspace.getConfiguration({
       scopeUri: resource,
-      section: 'sourcepawnLanguageServer'
+      section: "sourcepawnLanguageServer",
     });
     documentSettings.set(resource, result);
   }
@@ -106,11 +103,11 @@ function getDocumentSettings(resource: string): Thenable<SourcepawnSettings> {
 }
 
 connection.onCompletion((textDocumentPosition) => {
-    return completions.get_completions(textDocumentPosition);
+  return completions.get_completions(textDocumentPosition);
 });
 
 connection.onSignatureHelp((textDocumentPosition) => {
-    return completions.get_signature(textDocumentPosition);
+  return completions.get_signature(textDocumentPosition);
 });
 
 connection.listen();
