f8a3ee19db7b549e6b39c0cb3e639a710b33702c
Sarrus1 <omegasix0611@gmail.com>
2021-04-14 18:06:05 +0200

    Fixed the tests

--- client/out/spMode.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SM_MODE = void 0;
-exports.SM_MODE = { language: 'sourcepawn', scheme: 'file' };
-//# sourceMappingURL=spMode.js.map
