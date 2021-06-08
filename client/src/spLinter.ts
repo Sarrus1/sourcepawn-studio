a0eb18434b96d4c5a2e1b1c427402ee8a3f31cb4
Sarrus1 <omegasix0611@gmail.com>
2021-06-08 23:23:50 +0200

    Switched to javascript object for error details

--- client/src/spLinter.ts
+++ client/src/spLinter.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import * as fs from "fs";
 import { execFile } from "child_process";
 import { URI } from "vscode-uri";
-import { errorDetails } from "./spIndex";
+import { errorDetails } from "./Misc/errorMessages";
 
 let myExtDir: string = vscode.extensions.getExtension(
   "Sarrus.sourcepawn-vscode"
