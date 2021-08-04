d2446e63d7af924fe792bbb6473e95ddfa68c0c4
Sarrus1 <omegasix0611@gmail.com>
2021-08-04 11:58:13 +0200

    Tests fix

--- client/src/test/runTest.ts
+++ client/src/test/runTest.ts
@@ -1,5 +1,4 @@
 import * as path from "path";
-import * as vscode from "vscode";
 import { runTests } from "vscode-test";
 
 async function main() {
--- client/src/test/suite/extension.test.ts
+++ client/src/test/suite/extension.test.ts
@@ -2,17 +2,12 @@ import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
 import * as fs from "fs";
 import {join} from "path";
 import {run as CreateTaskCommand} from "../../Commands/createTask";
 import {run as CreateScriptCommand} from "../../Commands/createScript";
 import {run as CreateREADMECommand} from "../../Commands/createREADME";
 import {run as CreateMasterCommand} from "../../Commands/createGitHubActions";
-import {run as CreateProjectCommand} from "../../Commands/createProject";
-import {run as CompileSMCommand} from "../../Commands/compileSM";
-
-import { after } from 'mocha';
 
 const testFolderLocation = '/../../../src/test/examples/'
 
@@ -21,7 +16,7 @@ suite('Extension Test', () => {
   test('Create Task Command', async () => {
 		let examplesVscode = join(__dirname, testFolderLocation, ".vscode");
 		rmdir(examplesVscode);
-		let error : number =	CreateTaskCommand("");
+		let error : number =	CreateTaskCommand();
 		// If sm_home is not defined, this command will error out.
 		// This counts this error as expected behaviour.
 		let test: boolean = error==0 || error==1
@@ -32,7 +27,7 @@ suite('Extension Test', () => {
 	test('Create Script Command', async () => {
 		let examplesScripting= join(__dirname, testFolderLocation, "scripting");
 		rmdir(examplesScripting);
-		let error : number=	CreateScriptCommand("");
+		let error : number=	CreateScriptCommand();
 		assert.equal(error, 0);
 		rmdir(examplesScripting);
 	});
@@ -42,7 +37,7 @@ suite('Extension Test', () => {
 		if (fs.existsSync(examplesReadme)) {
 			fs.unlinkSync(examplesReadme);
 		 }
-		let error : number=	CreateREADMECommand("");
+		let error : number=	CreateREADMECommand();
 		assert.equal(error, 0);
 		if (fs.existsSync(examplesReadme)) {
 			fs.unlinkSync(examplesReadme);
@@ -52,7 +47,7 @@ suite('Extension Test', () => {
 	test('Create Master Command', async () => {
 		let examplesGithub= join(__dirname, testFolderLocation, ".github");
 		rmdir(examplesGithub);
-		let error : number=	CreateMasterCommand("");
+		let error : number=	CreateMasterCommand();
 		assert.equal(error, 0);
 		rmdir(examplesGithub);
 	});
