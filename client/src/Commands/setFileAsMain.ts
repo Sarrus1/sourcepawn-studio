72ee70f30f787632b0e7c62a3b8f63e67e02318c
Sarrus1 <omegasix0611@gmail.com>
2021-10-15 22:06:31 +0200

    Set file as main is now scopped

--- client/src/Commands/setFileAsMain.ts
+++ client/src/Commands/setFileAsMain.ts
@@ -2,10 +2,14 @@ import { workspace as Workspace, window, Uri } from "vscode";
 
 export async function run(args: any) {
   let uri: Uri = args.document.uri;
+  let workspaceFolder = Workspace.getWorkspaceFolder(uri);
   if (uri === undefined) {
     window.showErrorMessage("No file are selected");
     return 1;
   }
-  Workspace.getConfiguration("sourcepawn").update("MainPath", uri.fsPath);
+  Workspace.getConfiguration("sourcepawn", workspaceFolder).update(
+    "MainPath",
+    uri.fsPath
+  );
   return 0;
 }
