3a04bea7356b434337c6109f98c42af971c7f30a
Sarrus1 <omegasix0611@gmail.com>
2022-02-23 11:06:51 +0100

    Chore: Add documentation

--- src/Parser/addVariableItem.ts
+++ src/Parser/addVariableItem.ts
@@ -4,6 +4,21 @@ import { PropertyItem } from "../Backend/Items/spPropertyItem";
 import { State } from "./stateEnum";
 import { globalIdentifier } from "../Misc/spConstants";
 
+/**
+ * Save a variable and generate the appropriate key for the Map it is stored in.
+ * The key is a concatenation of the following variables:
+ * - *varName*: The name of the variable
+ * - *scope*: The scope of the variable (the last function's name or globalIdentifier)
+ * - *enumStructName*: The name of the enum struct (empty if none)
+ * - *lastFuncName*: The name of the last function (empty if none)
+ * @param  {Parser} parser
+ * @param  {string} name
+ * @param  {string} line
+ * @param  {string} type
+ * @param  {string} funcName?
+ * @param  {} isParamDef=false
+ * @returns void
+ */
 export function addVariableItem(
   parser: Parser,
   name: string,
