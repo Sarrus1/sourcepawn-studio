66bec273db1ce583e8bc35c3857906f3f1ac6131
Sarrus1 <omegasix0611@gmail.com>
2022-05-20 16:43:16 +0200

    Chore: Code cleanup

--- src/Parser/PreProcessor/spPreprocessor.ts
+++ src/Parser/PreProcessor/spPreprocessor.ts
@@ -39,154 +39,33 @@ export class PreProcessor {
     this.preprocessedLines += line + "\n";
   }
 
-  private nextLine() {
-    this.lineNb++;
-    if (this.lines.length <= this.lineNb) {
-      this.line = undefined;
-    }
-    this.line = this.lines[this.lineNb];
-  }
-
   public preProcess(): string {
     for (let line of this.lines) {
       let match = line.match(/^\s*#define\s+([A-Za-z_]\w*)[^\S\r\n]*/);
 
       if (match) {
-        // Add the line no matter what, to get define autocompletion.
-        this.addLine(line);
-
-        const defineValSt = match.index + match[0].length;
-        if (line.length <= defineValSt) {
-          continue;
-        }
-        const lineState: LineState = {
-          openedQuote: Quote.None,
-          blockComment: false,
-        };
-        let value = "";
-        for (let i = defineValSt; i < line.length; i++) {
-          if (!lineState.blockComment && lineState.openedQuote === Quote.None) {
-            // No comment, no string.
-            if (i + 1 >= line.length) {
-              // Too short to continue, append value and exit.
-              // TODO: Check if multiline.
-              value += line[i];
-              break;
-            }
-            if (line[i] === "/" && line[i] === "/") {
-              // Start of a line comment, exit.
-              break;
-            }
-            if (line[i] === "/" && line[i] === "*") {
-              // Start of a block comment, check if it spans the whole line.
-              // TODO: Implement logic.
-              break;
-            }
-            if (line[i] === '"') {
-              lineState.openedQuote = Quote.Double;
-            } else if (line[i] === "'") {
-              lineState.openedQuote = Quote.Single;
-            }
-            value += line[i];
-          }
-          if (lineState.openedQuote === Quote.Single) {
-            // Single quote string.
-            if (i + 1 >= line.length) {
-              if (line[i] === "\\") {
-                // Line continuation.
-              }
-              value += line[i];
-            }
-            if (line[i] === "'" && line[i - 1] !== "\\") {
-              lineState.openedQuote = Quote.None;
-            }
-            value += line[i];
-            continue;
-          }
-          if (lineState.openedQuote === Quote.Double) {
-            // Single quote string.
-            if (i + 1 >= line.length) {
-              if (line[i] === "\\") {
-                // Line continuation.
-              }
-              value += line[i];
-            }
-            if (line[i] === '"' && line[i - 1] !== "\\") {
-              lineState.openedQuote = Quote.None;
-            }
-            value += line[i];
-          }
-        }
-
-        this.defines.set(match[1], value.trim());
+        this.handleDefine(match, line);
         continue;
       }
 
       match = line.match(/^\s*#if/);
 
       if (match) {
-        this.conditionState = ConditionState.if;
-        let condition = line.slice(match.index + match[0].length);
-        const matches = condition.match(/[A-Za-z_]\w*/g);
-        if (matches) {
-          // FIXME: Handle "defined".
-          for (let subMatch of matches) {
-            let define = this.defines.get(subMatch);
-            if (define !== undefined) {
-              condition = condition.replace(subMatch, define);
-            }
-          }
-        }
-        const evaluation = eval(condition);
-        if (evaluation) {
-          this.conditionWasActivated = true;
-          this.skipLine = false;
-        }
-        this.addLine("");
+        this.handleIf(match, line, ConditionState.if);
         continue;
       }
 
       match = line.match(/^\s*#elseif/);
 
       if (match) {
-        this.conditionState = ConditionState.elseIf;
-        if (this.conditionWasActivated) {
-          this.skipLine = true;
-          this.addLine("");
-          continue;
-        }
-        let condition = line.slice(match.index + match[0].length);
-        const matches = condition.match(/[A-Za-z_]\w*/g);
-        if (matches) {
-          // FIXME: Handle "defined".
-          for (let subMatch of matches) {
-            let define = this.defines.get(subMatch);
-            if (define !== undefined) {
-              condition = condition.replace(subMatch, define);
-            }
-          }
-        }
-        const evaluation = eval(condition);
-        if (evaluation) {
-          this.conditionWasActivated = true;
-          this.skipLine = true;
-        }
-        this.addLine("");
+        this.handleIf(match, line, ConditionState.elseIf);
         continue;
       }
 
       match = line.match(/^\s*#else/);
 
       if (match) {
-        this.conditionState = ConditionState.else;
-        if (this.conditionWasActivated) {
-          this.skipLine = true;
-          this.addLine("");
-          continue;
-        }
-        this.conditionWasActivated = true;
-        this.skipLine = false;
-        this.addLine(line);
+        this.handleElse(line);
         continue;
       }
 
@@ -207,5 +86,118 @@ export class PreProcessor {
       }
     }
     return this.preprocessedLines;
+  }
+
+  private handleDefine(match: RegExpMatchArray, line: string) {
+    // Add the line no matter what, to get define autocompletion.
+    this.addLine(line);
+
+    const defineValSt = match.index + match[0].length;
+    if (line.length <= defineValSt) {
+      return;
+    }
+    const lineState: LineState = {
+      openedQuote: Quote.None,
+      blockComment: false,
+    };
+    let value = "";
+    for (let i = defineValSt; i < line.length; i++) {
+      if (!lineState.blockComment && lineState.openedQuote === Quote.None) {
+        // No comment, no string.
+        if (i + 1 >= line.length) {
+          // Too short to continue, append value and exit.
+          // TODO: Check if multiline.
+          value += line[i];
+          break;
+        }
+        if (line[i] === "/" && line[i] === "/") {
+          // Start of a line comment, exit.
+          break;
+        }
+        if (line[i] === "/" && line[i] === "*") {
+          // Start of a block comment, check if it spans the whole line.
+          // TODO: Implement logic.
+          break;
+        }
+        if (line[i] === '"') {
+          lineState.openedQuote = Quote.Double;
+        } else if (line[i] === "'") {
+          lineState.openedQuote = Quote.Single;
+        }
+        value += line[i];
+      }
+      if (lineState.openedQuote === Quote.Single) {
+        // Single quote string.
+        if (i + 1 >= line.length) {
+          if (line[i] === "\\") {
+            // Line continuation.
+          }
+          value += line[i];
+        }
+        if (line[i] === "'" && line[i - 1] !== "\\") {
+          lineState.openedQuote = Quote.None;
+        }
+        value += line[i];
+        continue;
+      }
+      if (lineState.openedQuote === Quote.Double) {
+        // Single quote string.
+        if (i + 1 >= line.length) {
+          if (line[i] === "\\") {
+            // Line continuation.
+          }
+          value += line[i];
+        }
+        if (line[i] === '"' && line[i - 1] !== "\\") {
+          lineState.openedQuote = Quote.None;
+        }
+        value += line[i];
+      }
+    }
+
+    this.defines.set(match[1], value.trim());
+  }
+
+  private handleIf(
+    match: RegExpMatchArray,
+    line: string,
+    state: ConditionState
+  ) {
+    this.conditionState = state;
+    if (this.conditionWasActivated) {
+      this.skipLine = true;
+      this.addLine("");
+      return;
+    }
+    let condition = line.slice(match.index + match[0].length);
+    const matches = condition.match(/[A-Za-z_]\w*/g);
+    if (matches) {
+      // FIXME: Handle "defined".
+      for (let subMatch of matches) {
+        let define = this.defines.get(subMatch);
+        if (define !== undefined) {
+          condition = condition.replace(subMatch, define);
+        }
+      }
+    }
+    const evaluation = eval(condition);
+    if (evaluation) {
+      this.conditionWasActivated = true;
+      this.skipLine = false;
+    }
+    this.addLine("");
+    return;
+  }
+
+  private handleElse(line: string) {
+    this.conditionState = ConditionState.else;
+    if (this.conditionWasActivated) {
+      this.skipLine = true;
+      this.addLine("");
+      return;
+    }
+    this.conditionWasActivated = true;
+    this.skipLine = false;
+    this.addLine(line);
   }
 }
