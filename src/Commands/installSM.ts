832243814b13f4e38e4e64de86b5674a550f4432
BoomShot <75021396+BoomShotKapow@users.noreply.github.com>
2022-08-22 16:27:01 -0500

    Change installSM to use the latest SourceMod

--- src/Commands/installSM.ts
+++ src/Commands/installSM.ts
@@ -68,50 +68,75 @@ async function downloadSM(
   token: CancellationToken
 ): Promise<void> {
   return new Promise<void>((resolve, reject) => {
-    let oldStatus: number = 0;
-    let src: string;
-    if (Platform === "win32") {
-      src = "http://sourcemod.net/latest.php?os=windows&version=1.10";
-    } else if (Platform === "darwin") {
-      src = "http://sourcemod.net/latest.php?os=mac&version=1.10";
-    } else {
-      src = "http://sourcemod.net/latest.php?os=linux&version=1.10";
-    }
-    const output = join(outputDir, "sm.gz");
     const options = {
-      gunzip: false,
+      protocol: 'https',
+      host: 'sm.alliedmods.net',
+      path: '',
+      method: 'GET'
     };
-    if (token.isCancellationRequested) {
-      return;
+
+    if (Platform === "win32") {
+      options.path = "/smdrop/1.11/sourcemod-latest-windows";
+    } else if (Platform === "darwin") {
+      options.path = "/smdrop/1.11/sourcemod-latest-mac";
+    } else {
+      options.path = "/smdrop/1.11/sourcemod-latest-linux";
     }
-    const download = wget.download(src, output, options);
-    download.on("error", function (err) {
-      console.error(err);
-      reject(err);
-    });
-    download.on("start", function (fileSize: number) {
-      console.log("filesize: ", Math.ceil(fileSize / Math.pow(10, 6)), "Mo");
-    });
-    download.on("end", async function (endStatus) {
-      console.log(endStatus);
-      progress.report({ message: "Unzipping..." });
-      await decompress(output, outputDir);
-      resolve(endStatus);
-    });
-    download.on("progress", function (status) {
-      if (typeof status === "number") {
-        status = Math.floor(status * 100);
-        const inc = status - oldStatus;
-        oldStatus = status;
-        progress.report({
-          message: "Downloading Sourcemod",
-          increment: inc,
+
+    let request = wget.request(options, function(response) {
+      let oldStatus: number = 0;
+      let content = '';
+      if (response.statusCode === 200) {
+        response.on('error', function(err) {
+          console.log(err);
         });
+        response.on('data', function(chunk) {
+          content += chunk;
+        });
+        response.on('end', function() {
+          progress.report({ message: "Downloading SourceMod: " + content });
+          console.log(content);
+
+          const output = join(outputDir, "sm.gz");
+
+          const download = wget.download("https://sm.alliedmods.net/smdrop/1.11/" + content, output, options);
+          download.on("error", function (err) {
+            console.error(err);
+            reject(err);
+          });
+          download.on("start", function (fileSize: number) {
+            console.log("filesize: ", Math.ceil(fileSize / Math.pow(10, 6)), "Mo");
+          });
+          download.on("end", async function (endStatus) {
+            console.log(endStatus);
+            progress.report({ message: "Unzipping..." });
+            await decompress(output, outputDir);
+            resolve(endStatus);
+          });
+          download.on("progress", function (status) {
+            if (typeof status === "number") {
+              status = Math.floor(status * 100);
+              const inc = status - oldStatus;
+              oldStatus = status;
+              progress.report({
+                message: "Downloading Sourcemod " + content,
+                increment: inc,
+              });
+            }
+          });
+          token.onCancellationRequested(() => {
+            console.log("SourceMod download was cancelled by the user.");
+            //TODO: Actually stop the download here. Might need a better NPM package...
+          });
+        });
+      } else {
+        console.log('Response: ' + response.statusCode);
       }
     });
-    token.onCancellationRequested(() => {
-      console.log("Sourcemod download was cancelled by the user.");
-      //TODO: Actually stop the download here. Might need a better NPM package...
+
+    request.end();
+    request.on('error', function(err) {
+      console.log(err);
     });
   });
 }
