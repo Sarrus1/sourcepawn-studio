106d43f57c58a8bc1b4206bd875382b6a672321b
Suza <zabaniya101@gmail.com>
2022-11-20 19:28:54 +0100

    Update the scripts to handle external include files

---
+++ templates/dependencies.sh
@@ -0,0 +1,16 @@
+# If you want to download one specific include file, use wget and utilize the Raw link of the file 
+# ( click the file and there will be a Raw button at the top right of the code ).
+#
+# E.g.
+# wget "https://raw.githubusercontent.com/alliedmodders/sourcemod/master/plugins/include/sdkhooks.inc"
+#
+# If the include file is supposed to be in a subdirectory ( e.g. include/test/more.inc ),
+# remember to use mkdir to create the folder and then cd to change to that directory.
+#
+# E.g.
+# mkdir test
+# cd test
+# wget ...
+#
+#
+# As of now, if you want to download an entire directory you'll have to manually download each include file.
