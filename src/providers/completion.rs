ea527e722a1d4d497d4f769b14e9e1acd3e86339
Sarrus1 <omegasix0611@gmail.com>
2022-11-24 14:13:56 +0100

    feat: recursively get all items

--- src/providers/completion.rs
+++ src/providers/completion.rs
@@ -1,17 +1,18 @@
-use lsp_types::{CompletionItem, CompletionList, CompletionParams};
+use lsp_types::{CompletionItem, CompletionList, CompletionParams, Url};
 
-use crate::spitem::to_completion;
+use crate::spitem::{get_all_items, to_completion};
 
 use super::FeatureRequest;
 
 pub fn provide_completions(request: FeatureRequest<CompletionParams>) -> Option<CompletionList> {
+    let main_path = request.store.environment.options.main_path.clone();
+    let main_path_uri = Url::from_file_path(main_path).expect("Invalid main path");
+    let all_items = get_all_items(&request.store, main_path_uri);
     let mut items: Vec<CompletionItem> = Vec::new();
-    for (_, file_item) in request.store.documents.iter() {
-        for sp_item in file_item.sp_items.iter() {
-            let res = to_completion(sp_item, &request.params);
-            if res.is_some() {
-                items.push(res.unwrap());
-            }
+    for sp_item in all_items.iter() {
+        let res = to_completion(sp_item, &request.params);
+        if res.is_some() {
+            items.push(res.unwrap());
         }
     }
 
--- src/spitem.rs
+++ src/spitem.rs
@@ -1,4 +1,8 @@
-use lsp_types::{CompletionItem, CompletionParams};
+use std::collections::HashSet;
+
+use lsp_types::{CompletionItem, CompletionParams, Url};
+
+use crate::{document::Document, store::Store};
 
 use self::function_item::FunctionItem;
 
@@ -14,5 +18,32 @@ pub enum SPItem {
 pub fn to_completion(sp_item: &SPItem, params: &CompletionParams) -> Option<CompletionItem> {
     match sp_item {
         SPItem::Function(function_item) => function_item::to_completion(function_item, params),
+    }
+}
+
+pub fn get_all_items(store: &Store, main_path_uri: Url) -> Vec<SPItem> {
+    let mut includes: HashSet<Url> = HashSet::new();
+    includes.insert(main_path_uri.clone());
+    let mut all_items = vec![];
+    if let Some(document) = store.get(&main_path_uri) {
+        get_included_files(store, document, &mut includes);
+        for include in includes.iter() {
+            let document = store.get(include).unwrap();
+            all_items.extend(document.sp_items);
+        }
+    }
+
+    all_items
+}
+
+fn get_included_files(store: &Store, document: Document, includes: &mut HashSet<Url>) {
+    for include_uri in document.includes.iter() {
+        if includes.contains(include_uri) {
+            return;
+        }
+        includes.insert(include_uri.clone());
+        if let Some(include_document) = store.get(include_uri) {
+            get_included_files(store, include_document, includes);
+        }
     }
 }
