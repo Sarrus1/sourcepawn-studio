18e28ebde7d11c5aff7a15a4776fff4c2ef3a3d3
Sarrus1 <omegasix0611@gmail.com>
2022-12-16 23:56:40 +0100

    chore: documentation

--- src/providers/completion.rs
+++ src/providers/completion.rs
@@ -1,10 +1,10 @@
-use lsp_types::{CompletionItem, CompletionList, CompletionParams, Position};
+use lsp_types::{CompletionList, CompletionParams};
 
-use crate::spitem::{get_all_items, get_items_from_position, SPItem};
+use crate::spitem::get_all_items;
 
 use self::{
-    context::{get_line_words, is_method_call},
-    getters::get_children_of_mm_or_es,
+    context::is_method_call,
+    getters::{get_method_completions, get_non_method_completions},
 };
 
 use super::FeatureRequest;
@@ -20,76 +20,8 @@ pub fn provide_completions(request: FeatureRequest<CompletionParams>) -> Option<
     let line = document.line(position.line)?;
 
     if !is_method_call(line, position) {
-        let mut items: Vec<CompletionItem> = Vec::new();
-        for sp_item in all_items.iter() {
-            let res = sp_item
-                .lock()
-                .unwrap()
-                .to_completion(&request.params, false);
-            if let Some(res) = res {
-                items.push(res);
-            }
-        }
-
-        return Some(CompletionList {
-            items,
-            ..Default::default()
-        });
+        return get_non_method_completions(all_items, request.params);
     }
 
-    let words = get_line_words(line, position);
-    for word in words.into_iter().flatten().rev() {
-        let word_pos = Position {
-            line: word.range.start.line,
-            character: ((word.range.start.character + word.range.end.character) / 2),
-        };
-        let items = get_items_from_position(
-            &request.store,
-            word_pos,
-            request
-                .params
-                .text_document_position
-                .text_document
-                .uri
-                .clone(),
-        );
-        if items.is_empty() {
-            continue;
-        }
-        for item in items.iter() {
-            let type_ = item.lock().unwrap().type_();
-            for item_ in all_items.iter() {
-                if item_.lock().unwrap().name() == type_ {
-                    let item_lock = item_.lock().unwrap().clone();
-                    match item_lock {
-                        SPItem::Methodmap(mm_item) => {
-                            return Some(CompletionList {
-                                // TODO: Handle inherit here
-                                // TODO: Handle static methods
-                                items: get_children_of_mm_or_es(
-                                    &all_items,
-                                    mm_item.name,
-                                    request.params,
-                                ),
-                                ..Default::default()
-                            });
-                        }
-                        SPItem::EnumStruct(es_item) => {
-                            return Some(CompletionList {
-                                items: get_children_of_mm_or_es(
-                                    &all_items,
-                                    es_item.name,
-                                    request.params,
-                                ),
-                                ..Default::default()
-                            });
-                        }
-                        _ => {}
-                    }
-                }
-            }
-        }
-    }
-
-    None
+    get_method_completions(all_items, line, position, request)
 }
--- src/providers/completion/getters.rs
+++ src/providers/completion/getters.rs
@@ -1,8 +1,13 @@
 use std::sync::{Arc, Mutex};
 
-use lsp_types::{CompletionItem, CompletionParams};
+use lsp_types::{CompletionItem, CompletionList, CompletionParams, Position};
 
-use crate::spitem::SPItem;
+use crate::{
+    providers::FeatureRequest,
+    spitem::{get_items_from_position, SPItem},
+};
+
+use super::context::get_line_words;
 
 /// Search in a vector of items for the childs of a type and return the associated
 /// vector of [CompletionItem](lsp_types::CompletionItem).
@@ -31,4 +36,101 @@ pub(super) fn get_children_of_mm_or_es(
     }
 
     res
+}
+
+/// Return a [CompletionList](lsp_types::CompletionList) of all non method completions (that don't come
+/// after a `.` or `::`).
+///
+/// # Arguments
+///
+/// * `all_item` - Vector of [SPItem](crate::spitem::SPItem).
+/// * `params` - [Parameters](lsp_types::completion::CompletionParams) of the completion request.
+pub(super) fn get_non_method_completions(
+    all_items: Vec<Arc<Mutex<SPItem>>>,
+    params: CompletionParams,
+) -> Option<CompletionList> {
+    let mut items: Vec<CompletionItem> = Vec::new();
+    for sp_item in all_items.iter() {
+        let res = sp_item.lock().unwrap().to_completion(&params, false);
+        if let Some(res) = res {
+            items.push(res);
+        }
+    }
+
+    Some(CompletionList {
+        items,
+        ..Default::default()
+    })
+}
+
+/// Return a [CompletionList](lsp_types::CompletionList) of all method completions (that should come
+/// after a `.` or `::`).
+///
+/// # Arguments
+///
+/// * `all_item` - Vector of [SPItem](crate::spitem::SPItem).
+/// * `line` - Line of the document to analyze.
+/// * `position` - [Position](lsp_types::Position) of the request.
+/// * `params` - [Parameters](lsp_types::completion::CompletionParams) of the completion request.
+pub(super) fn get_method_completions(
+    all_items: Vec<Arc<Mutex<SPItem>>>,
+    line: &str,
+    position: Position,
+    request: FeatureRequest<CompletionParams>,
+) -> Option<CompletionList> {
+    let words = get_line_words(line, position);
+    for word in words.into_iter().flatten().rev() {
+        let word_pos = Position {
+            line: word.range.start.line,
+            character: ((word.range.start.character + word.range.end.character) / 2),
+        };
+        let items = get_items_from_position(
+            &request.store,
+            word_pos,
+            request
+                .params
+                .text_document_position
+                .text_document
+                .uri
+                .clone(),
+        );
+        if items.is_empty() {
+            continue;
+        }
+        for item in items.iter() {
+            let type_ = item.lock().unwrap().type_();
+            for item_ in all_items.iter() {
+                if item_.lock().unwrap().name() == type_ {
+                    let item_lock = item_.lock().unwrap().clone();
+                    match item_lock {
+                        SPItem::Methodmap(mm_item) => {
+                            return Some(CompletionList {
+                                // TODO: Handle inherit here
+                                // TODO: Handle static methods
+                                items: get_children_of_mm_or_es(
+                                    &all_items,
+                                    mm_item.name,
+                                    request.params,
+                                ),
+                                ..Default::default()
+                            });
+                        }
+                        SPItem::EnumStruct(es_item) => {
+                            return Some(CompletionList {
+                                items: get_children_of_mm_or_es(
+                                    &all_items,
+                                    es_item.name,
+                                    request.params,
+                                ),
+                                ..Default::default()
+                            });
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    None
 }
