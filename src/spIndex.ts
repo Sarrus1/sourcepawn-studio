d392485b8cc96bcd445a884f7b500e3cecd2c13a
Sarrus1 <omegasix0611@gmail.com>
2023-01-28 00:01:59 +0100

    feat: add lsp binaries

--- src/spIndex.ts
+++ src/spIndex.ts
@@ -1,17 +1,33 @@
 import { homedir } from "os";
 import { join } from "path";
-import { workspace, ExtensionContext, languages } from "vscode";
+import { workspace, ExtensionContext, languages, extensions } from "vscode";
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
 } from "vscode-languageclient/node";
+import { platform, arch } from "os";
 
 import { registerSMCommands } from "./Commands/registerCommands";
 import { SMDocumentFormattingEditProvider } from "./Formatters/spFormat";
 import { KVDocumentFormattingEditProvider } from "./Formatters/kvFormat";
 
 let client: LanguageClient;
+
+function makeCommand() {
+  let lsp_path = join(
+    extensions.getExtension("Sarrus.sourcepawn-vscode").extensionPath,
+    "bin"
+  );
+  const platform_ = platform();
+  const arch_ = arch();
+  if (platform_ === "win32") {
+    lsp_path = join(lsp_path, "win32/sourcepawn_lsp.exe");
+  } else {
+    lsp_path = "./" + join(lsp_path, `${platform_}_${arch_}/sourcepawn_lsp`);
+  }
+  return lsp_path;
+}
 
 export function activate(context: ExtensionContext) {
   registerSMCommands(context);
@@ -35,17 +51,18 @@ export function activate(context: ExtensionContext) {
     )
   );
 
-  const command = "cargo";
-  const manifestPath = join(homedir(), "dev/sourcepawn-lsp/Cargo.toml");
-  const args = ["run", "--manifest-path", manifestPath];
   const serverOptions: ServerOptions = {
     run: {
-      command,
-      args,
+      command: makeCommand(),
+      args: [],
     },
     debug: {
-      command,
-      args,
+      command: "cargo",
+      args: [
+        "run",
+        "--manifest-path",
+        join(homedir(), "dev/sourcepawn-lsp/Cargo.toml"),
+      ],
     },
   };
 
