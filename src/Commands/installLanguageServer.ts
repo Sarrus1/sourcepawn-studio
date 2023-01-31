d07dac23b696a70ff6547aea3e026f113c5f4974
Sarrus1 <omegasix0611@gmail.com>
2023-01-31 18:43:56 +0100

    feat: add download error handling

--- src/Commands/installLanguageServer.ts
+++ src/Commands/installLanguageServer.ts
@@ -6,7 +6,7 @@ import {
   extensions,
 } from "vscode";
 import { join } from "path";
-import { platform } from "os";
+import { arch, platform } from "os";
 import {
   createReadStream,
   createWriteStream,
@@ -59,16 +59,39 @@ export async function getLatestVersionName() {
 
 function makeLanguageServerURL(version: string) {
   const platform_ = platform();
-  let extension = "";
+  const arch_ = arch();
+  let platform_id = "";
+  let arch_id = "";
   if (platform_ === "win32") {
-    extension = "pc-windows-gnu";
+    platform_id = "windows";
+    if (arch_ === "x64") {
+      arch_id = "x86_64";
+    } else if (arch_ === "x32") {
+      arch_id = "i686";
+    } else if (arch_ === "arm64") {
+      arch_id = "aarch64";
+    }
   } else if (platform_ === "darwin") {
-    extension = "apple-darwin";
+    platform_id = "macos";
+    if (arch_ === "x64") {
+      arch_id = "x86_64";
+    } else if (arch_ === "arm64") {
+      arch_id = "aarch64";
+    }
   } else {
-    extension = "unknown-linux-musl";
+    platform_id = "linux";
+    if (arch_ === "x64") {
+      arch_id = "x86_64";
+    } else if (arch_ === "x32") {
+      arch_id = "i686";
+    } else if (arch_ === "arm64") {
+      arch_id = "aarch64";
+    } else {
+      arch_id = "armv7hf";
+    }
   }
 
-  return `https://github.com/Sarrus1/sourcepawn-lsp/releases/download/${version}/sourcepawn-lsp_${version}_x86_64-${extension}.zip`;
+  return `https://github.com/Sarrus1/sourcepawn-lsp/releases/download/${version}/sourcepawn_lsp-${arch_id}-${platform_id}.zip`;
 }
 
 async function downloadLanguageServer(
@@ -76,62 +99,74 @@ async function downloadLanguageServer(
   progress: Progress<{ message?: string; increment?: number }>,
   token: CancellationToken
 ) {
-  const { data, headers } = await axios({
-    url: makeLanguageServerURL(version),
-    method: "GET",
-    responseType: "stream",
-  });
-  const lspPath = join(
-    extensions.getExtension("Sarrus.sourcepawn-vscode").extensionPath,
-    "languageServer"
-  );
-  const zipPath = join(lspPath, "sourcepawn_lsp.zip");
-
-  return new Promise<void>((resolve, reject) => {
-    const totalLength = headers["content-length"];
-
-    const writer = createWriteStream(zipPath);
-
-    // Update the progress bar.
-    let oldStatus = 0;
-    let newStatus = 0;
-    let counter = 0;
-    data.on("data", (chunk) => {
-      counter += chunk.length;
-      oldStatus = newStatus;
-      newStatus = Math.floor((counter / totalLength) * 100);
-      let inc = newStatus - oldStatus;
-      progress.report({
-        message: "Downloading...",
-        increment: inc,
-      });
+  try {
+    const { data, headers } = await axios({
+      url: makeLanguageServerURL(version),
+      method: "GET",
+      responseType: "stream",
     });
-    data.pipe(writer);
 
-    writer.on("finish", () => {
-      // Unzip the file now that it has been downloaded.
-      progress.report({
-        message: "Unzipping...",
-      });
-      const artifactName =
-        platform() == "win32" ? "sourcepawn_lsp.exe" : "sourcepawn_lsp";
-      const outPath = join(lspPath, artifactName);
-      createReadStream(zipPath)
-        .pipe(unzipper.Parse())
-        .on("entry", function (entry) {
-          if (entry.path === artifactName) {
-            entry.pipe(createWriteStream(outPath));
-          } else {
-            entry.autodrain();
-          }
-        })
-        .on("close", () => {
-          if (platform() != "win32") {
-            execSync(`chmod +x ${outPath}`);
-          }
-          rmSync(zipPath);
-          resolve();
+    const lspPath = join(
+      extensions.getExtension("Sarrus.sourcepawn-vscode").extensionPath,
+      "languageServer"
+    );
+    const zipPath = join(lspPath, "sourcepawn_lsp.zip");
+
+    return new Promise<void>((resolve, reject) => {
+      const totalLength = headers["content-length"];
+
+      const writer = createWriteStream(zipPath);
+
+      // Update the progress bar.
+      let oldStatus = 0;
+      let newStatus = 0;
+      let counter = 0;
+      data.on("data", (chunk) => {
+        counter += chunk.length;
+        oldStatus = newStatus;
+        newStatus = Math.floor((counter / totalLength) * 100);
+        let inc = newStatus - oldStatus;
+        progress.report({
+          message: "Downloading...",
+          increment: inc,
         });
+      });
+      data.pipe(writer);
+
+      writer.on("finish", () => {
+        // Unzip the file now that it has been downloaded.
+        progress.report({
+          message: "Unzipping...",
+        });
+        const artifactName =
+          platform() == "win32" ? "sourcepawn_lsp.exe" : "sourcepawn_lsp";
+        const outPath = join(lspPath, artifactName);
+        createReadStream(zipPath)
+          .pipe(unzipper.Parse())
+          .on("entry", function (entry) {
+            if (entry.path === artifactName) {
+              entry.pipe(createWriteStream(outPath));
+            } else {
+              entry.autodrain();
+            }
+          })
+          .on("close", () => {
+            if (platform() != "win32") {
+              execSync(`chmod +x ${outPath}`);
+            }
+            rmSync(zipPath);
+            resolve();
+          });
+      });
     });
-  });
+  } catch (error) {
+    if (
+      error["response"] !== undefined &&
+      error["response"]["status"] === 404
+    ) {
+      window.showErrorMessage(
+        "No release of sourcepawn_lsp were found for your. Either your OS is not supported or the release is currently being built. Please check the status [here](https://github.com/Sarrus1/sourcepawn-lsp/releases/latest)."
+      );
+    }
+  }
 }
