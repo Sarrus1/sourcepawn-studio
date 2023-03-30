d1a6e6e7d5203b3155ff977e10908d288a5e6213
Sarrus1 <omegasix0611@gmail.com>
2023-03-30 21:45:57 +0200

    fix: compile command

--- src/Commands/compileSM.ts
+++ src/Commands/compileSM.ts
@@ -114,17 +114,19 @@ export async function run(args: URI): Promise<void> {
   ).get("compilerOptions");
 
   const includePaths: string[] = [
-    Workspace.getConfiguration("sourcepawn", workspaceFolder).get(
-      "SourcemodHome"
-    ),
     join(scriptingFolderPath, "include"),
     scriptingFolderPath,
   ];
 
-  // Add the optional includes folders.
-  getAllPossibleIncludeFolderPaths(URI.file(fileToCompilePath), true).forEach(
-    (e) => includePaths.push(e)
-  );
+  Workspace.getConfiguration("SourcePawnLanguageServer", workspaceFolder)
+    .get<string[]>("includesDirectories")
+    .map((e) =>
+      resolve(
+        workspaceFolder === undefined ? "" : workspaceFolder.uri.fsPath,
+        e
+      )
+    )
+    .forEach((e) => includePaths.push(e));
 
   let compilerArgs = [fileToCompilePath, `-o${outputDir}`];
 
@@ -173,45 +175,4 @@ export async function run(args: URI): Promise<void> {
   } catch (error) {
     console.error(error);
   }
-}
-
-/**
- * Return all the possible include directories paths, such as SMHome, etc. The function will only return existing paths.
- * @param  {URI} uri                          The URI of the file from which we are trying to read the include.
- * @param  {boolean} onlyOptionalPaths        Whether or not the function only return the optionalIncludeFolderPaths.
- * @returns string
- */
-export function getAllPossibleIncludeFolderPaths(
-  uri: URI,
-  onlyOptionalPaths = false
-): string[] {
-  let possibleIncludePaths: string[] = [];
-  const workspaceFolder = Workspace.getWorkspaceFolder(uri);
-
-  possibleIncludePaths = Workspace.getConfiguration(
-    "sourcepawn",
-    workspaceFolder
-  ).get("optionalIncludeDirsPaths");
-  possibleIncludePaths = possibleIncludePaths.map((e) =>
-    resolve(workspaceFolder === undefined ? "" : workspaceFolder.uri.fsPath, e)
-  );
-
-  if (onlyOptionalPaths) {
-    return possibleIncludePaths;
-  }
-
-  const smHome = Workspace.getConfiguration(
-    "sourcepawn",
-    workspaceFolder
-  ).get<string>("SourcemodHome");
-
-  if (smHome !== undefined) {
-    possibleIncludePaths.push(smHome);
-  }
-
-  const scriptingFolder = dirname(uri.fsPath);
-  possibleIncludePaths.push(scriptingFolder);
-  possibleIncludePaths.push(join(scriptingFolder, "include"));
-
-  return possibleIncludePaths.filter((e) => e !== "" && existsSync(e));
 }
