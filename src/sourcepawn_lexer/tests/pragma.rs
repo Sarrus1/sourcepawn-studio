3042de380a930a9d4c72ab05bfbab42a999299f1
Sarrus1 <omegasix0611@gmail.com>
2023-04-07 19:58:18 +0200

    feat: add pragma tests

---
+++ src/sourcepawn_lexer/tests/pragma.rs
@@ -0,0 +1,142 @@
+use logos::Logos;
+use sourcepawn_lexer::Token;
+
+#[test]
+fn simple_pragma() {
+    let input = r#"#pragma deprecated foo
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..22);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 22..23);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_trailing_line_comment() {
+    let input = r#"#pragma deprecated foo //bar
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..23);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo ");
+
+    assert_eq!(lexer.next(), Some(Token::LineComment));
+    assert_eq!(lexer.span(), 23..28);
+    assert_eq!(lexer.slice(), "//bar");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 28..29);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_trailing_block_comment() {
+    let input = r#"#pragma deprecated foo /* */
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..23);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo ");
+
+    assert_eq!(lexer.next(), Some(Token::BlockComment));
+    assert_eq!(lexer.span(), 23..28);
+    assert_eq!(lexer.slice(), "/* */");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 28..29);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_with_block_comment() {
+    let input = r#"#pragma deprecated foo /* */ bar
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..32);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo /* */ bar");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 32..33);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_with_block_comment_and_line_continuation() {
+    let input = r#"#pragma deprecated foo /* */ \
+bar
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..34);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo /* */ \\\nbar");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 34..35);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_with_trailing_multiline_block_comment() {
+    let input = r#"#pragma deprecated foo /* 
+*/ bar
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..23);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo ");
+
+    assert_eq!(lexer.next(), Some(Token::BlockComment));
+    assert_eq!(lexer.span(), 23..29);
+    assert_eq!(lexer.slice(), "/* \n*/");
+
+    assert_eq!(lexer.next(), Some(Token::Identifier));
+    assert_eq!(lexer.span(), 30..33);
+    assert_eq!(lexer.slice(), "bar");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 33..34);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_with_trailing_line_continuated_multiline_block_comment() {
+    let input = r#"#pragma deprecated foo /* \
+*/ bar
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..34);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo /* \\\n*/ bar");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 34..35);
+    assert_eq!(lexer.slice(), "\n");
+}
+
+#[test]
+fn pragma_line_continuation() {
+    let input = r#"#pragma deprecated foo \
+bar
+"#;
+
+    let mut lexer = Token::lexer(input);
+    assert_eq!(lexer.next(), Some(Token::MPragma));
+    assert_eq!(lexer.span(), 0..28);
+    assert_eq!(lexer.slice(), "#pragma deprecated foo \\\nbar");
+
+    assert_eq!(lexer.next(), Some(Token::Newline));
+    assert_eq!(lexer.span(), 28..29);
+    assert_eq!(lexer.slice(), "\n");
+}
