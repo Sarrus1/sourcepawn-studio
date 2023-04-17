28be9c954803b224e15407a5ba9e750c0de0c027
Sarrus1 <omegasix0611@gmail.com>
2023-04-17 16:54:19 +0200

    feat: basic expression evaluator

--- src/sourcepawn_preprocessor/src/lib.rs
+++ src/sourcepawn_preprocessor/src/lib.rs
@@ -1,4 +1,4 @@
-use sourcepawn_lexer::{SourcepawnLexer, Symbol, TokenKind};
+use sourcepawn_lexer::{Literal, Operator, PreprocDir, SourcepawnLexer, Symbol, TokenKind};
 
 #[derive(Debug, Clone)]
 pub struct SourcepawnPreprocessor<'a> {
@@ -22,16 +22,15 @@ impl<'a> SourcepawnPreprocessor<'a> {
     pub fn preprocess_input(&mut self) -> String {
         while let Some(symbol) = self.lexer.next() {
             match symbol.token_kind {
-                // TokenKind::MIf => {
-                //     let mut if_condition = IfCondition::default();
-                //     while let Some(symbol) = lexer.next() {
-                //         if symbol.token_kind == TokenKind::Newline {
-                //             break;
-                //         }
-                //         if_condition.symbols.push(symbol);
-                //     }
-                //     println!("{:?}", if_condition.evaluate());
-                // }
+                TokenKind::PreprocDir(PreprocDir::MIf) => {
+                    let mut if_condition = IfCondition::default();
+                    while self.lexer.in_preprocessor() {
+                        if let Some(symbol) = self.lexer.next() {
+                            if_condition.symbols.push(symbol);
+                        }
+                    }
+                    eprintln!("Evaluate {}", if_condition.evaluate());
+                }
                 TokenKind::Newline => {
                     self.push_ws(&symbol);
                     self.push_current_line();
@@ -79,6 +78,16 @@ mod test {
         let mut preprocessor = SourcepawnPreprocessor::new(input);
         assert_eq!(preprocessor.preprocess_input(), input);
     }
+
+    #[test]
+    fn if_directive() {
+        let input = r#"
+        #if (1+1 == 2) && (1+1==2)
+        "#;
+
+        let mut preprocessor = SourcepawnPreprocessor::new(input);
+        preprocessor.preprocess_input();
+    }
 }
 
 #[derive(Default, Debug)]
@@ -86,110 +95,216 @@ pub struct IfCondition {
     symbols: Vec<Symbol>,
 }
 
-// impl IfCondition {
-//     fn evaluate(&self) -> bool {
-//         let mut output_queue: Vec<Symbol> = vec![];
-//         let mut operator_stack: Vec<Symbol> = vec![];
+impl IfCondition {
+    pub fn evaluate(&self) -> bool {
+        let val = self.yard();
+        eprintln!("Val: {}", val);
+        val != 0
+    }
 
-//         for symbol in &self.symbols {
-//             match symbol.token_kind {
-//                 TokenKind::Identifier => {
-//                     // if let Some(value) = macros.get(name) {
-//                     //     let value = value.parse::<bool>().unwrap_or(false);
-//                     //     stack.push(value);
-//                     // } else {
-//                     //     panic!("Undefined macro: {}", name);
-//                     // }
-//                 }
-//                 // TokenKind::Or | TokenKind::And => operator = Some(symbol.text.clone()),
-//                 TokenKind::Or
-//                 | TokenKind::And
-//                 | TokenKind::Equals
-//                 | TokenKind::NotEquals
-//                 | TokenKind::Lt
-//                 | TokenKind::Gt
-//                 | TokenKind::Le
-//                 | TokenKind::Ge => {
-//                     let precedence = match symbol.text.as_str() {
-//                         "==" | "!=" => 2,
-//                         "<" | ">" | "<=" | ">=" => 3,
-//                         "&&" => 4,
-//                         "||" => 5,
-//                         _ => panic!("Invalid operator: {:?}", &symbol),
-//                     };
-//                     while let Some(top) = operator_stack.last() {
-//                         let top_precedence = match top.text.as_str() {
-//                             "==" | "!=" => 2,
-//                             "<" | ">" | "<=" | ">=" => 3,
-//                             "&&" => 4,
-//                             "||" => 5,
-//                             _ => panic!("Invalid operator: {:?}", &top),
-//                         };
-//                         if top_precedence >= precedence {
-//                             output_queue.push(operator_stack.pop().unwrap());
-//                         } else {
-//                             break;
-//                         }
-//                     }
-//                     operator_stack.push(symbol.clone());
-//                 }
-//                 // TokenKind::Equals
-//                 // | TokenKind::NotEquals
-//                 // | TokenKind::Lt
-//                 // | TokenKind::Gt
-//                 // | TokenKind::Le
-//                 // | TokenKind::Ge => {
-//                 //     let right = stack.pop().unwrap_or(false);
-//                 //     let left = stack.pop().unwrap_or(false);
-//                 //     let result = match symbol.text.as_str() {
-//                 //         "==" => left == right,
-//                 //         "!=" => left != right,
-//                 //         "<" => left < right,
-//                 //         ">" => left > right,
-//                 //         "<=" => left <= right,
-//                 //         ">=" => left >= right,
-//                 //         _ => panic!("Invalid operator: {}", symbol.text),
-//                 //     };
-//                 //     stack.push(result);
-//                 // }
-//                 _ => output_queue.push(symbol.clone()),
-//             }
+    fn yard(&self) -> i32 {
+        let mut output_queue: Vec<i32> = vec![];
+        let mut operator_stack: Vec<PreOperator> = vec![];
+        let mut may_be_unary = true;
+        for symbol in &self.symbols {
+            match &symbol.token_kind {
+                TokenKind::LParen => {
+                    operator_stack.push(PreOperator::LParen);
+                    may_be_unary = true;
+                }
+                TokenKind::RParen => {
+                    while let Some(top) = operator_stack.last() {
+                        if PreOperator::LParen == *top {
+                            operator_stack.pop();
+                            may_be_unary = false;
+                            break;
+                        } else {
+                            process_op(&mut output_queue, &operator_stack.pop().unwrap());
+                        }
+                    }
+                }
+                TokenKind::Operator(op) => {
+                    let mut cur_op = PreOperator::from_op(op);
+                    if may_be_unary && is_unary(op) {
+                        cur_op = match op {
+                            Operator::Not => PreOperator::Not,
+                            Operator::Tilde => PreOperator::Tilde,
+                            Operator::Minus => PreOperator::Negate,
+                            Operator::Plus => PreOperator::Confirm,
+                            _ => unreachable!(),
+                        };
+                    }
+                    while let Some(top) = operator_stack.last() {
+                        if top == &PreOperator::LParen {
+                            break;
+                        }
+                        if (!cur_op.is_unary() && top.priority() <= cur_op.priority())
+                            || (cur_op.is_unary() && top.priority() < cur_op.priority())
+                        {
+                            process_op(&mut output_queue, &operator_stack.pop().unwrap());
+                        } else {
+                            break;
+                        }
+                    }
+                    operator_stack.push(cur_op);
+                    may_be_unary = true;
+                }
+                // TokenKind::True | TokenKind::False | TokenKind::Literal(_) => {
+                //     output_queue.push(symbol.clone())
+                // }
+                TokenKind::True => {
+                    output_queue.push(1);
+                    may_be_unary = false;
+                }
+                TokenKind::False => {
+                    output_queue.push(0);
+                    may_be_unary = false;
+                }
+                TokenKind::Literal(lit) => match lit {
+                    Literal::IntegerLiteral => {
+                        output_queue.push(symbol.text().parse().unwrap());
+                        may_be_unary = false;
+                    }
+                    _ => todo!("Literal: {:?}", lit),
+                },
+                TokenKind::Comment(_) | TokenKind::Newline => (),
+                _ => todo!("TokenKind: {:?}", &symbol.token_kind),
+            }
+        }
+        eprintln!("Output: {:?}", output_queue);
+        while !operator_stack.is_empty() {
+            process_op(&mut output_queue, &operator_stack.pop().unwrap());
+        }
 
-//             while let Some(op) = operator_stack.pop() {
-//                 output_queue.push(op);
-//             }
+        *output_queue.last().unwrap()
+    }
+}
 
-//             let mut stack: Vec<bool> = vec![];
+fn to_bool<T: std::cmp::PartialEq<i32>>(value: T) -> bool {
+    value != 0
+}
 
-//             for token in output_queue {
-//                 match token {
-//                     Token::Literal(value) => {
-//                         let value = value.parse::<bool>().unwrap_or(false);
-//                         stack.push(value);
-//                     }
-//                     Token::Operator(op) => {
-//                         let right = stack.pop().unwrap_or(false);
-//                         let left = stack.pop().unwrap_or(false);
-//                         let result = match op.as_str() {
-//                             "==" => left == right,
-//                             "!=" => left != right,
-//                             "<" => left < right,
-//                             ">" => left > right,
-//                             "<=" => left <= right,
-//                             ">=" => left >= right,
-//                             "&&" => left && right,
-//                             "||" => left || right,
-//                             _ => panic!("Invalid operator: {}", op),
-//                         };
-//                         stack.push(result);
-//                     }
-//                     _ => panic!("Invalid token in output queue: {:?}", token),
-//                 }
-//             }
+fn is_unary(op: &Operator) -> bool {
+    matches!(
+        op,
+        Operator::Not | Operator::Tilde | Operator::Minus | Operator::Plus
+    )
+}
 
-//             stack.pop().unwrap_or(false)
-//         }
+fn process_op(stack: &mut Vec<i32>, op: &PreOperator) {
+    let right = stack.pop().unwrap_or(0);
+    let left = stack.pop().unwrap_or(0);
+    let result: i32 = match op {
+        PreOperator::Equals => (left == right).into(),
+        PreOperator::NotEquals => (left != right).into(),
+        PreOperator::Lt => (left < right).into(),
+        PreOperator::Gt => (left > right).into(),
+        PreOperator::Le => (left <= right).into(),
+        PreOperator::Ge => (left >= right).into(),
+        PreOperator::Plus => left + right,
+        PreOperator::Minus => left - right,
+        PreOperator::Slash => left / right,
+        PreOperator::Star => left * right,
+        PreOperator::And => (to_bool(left) && to_bool(right)).into(),
+        PreOperator::Or => (to_bool(left) || to_bool(right)).into(),
+        _ => todo!("Operator: {:?}", op),
+    };
+    stack.push(result);
+}
 
-//         stack.pop().unwrap_or(false)
-//     }
-// }
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum PreOperator {
+    Not,
+    Tilde,
+
+    /// Unary `-`.
+    Negate,
+
+    /// Unary `+`.
+    Confirm,
+    Star,
+    Slash,
+    Percent,
+    Minus,
+    Plus,
+    Shl,
+    Shr,
+    Ushr,
+    Ampersand,
+    Bitxor,
+    Bitor,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+    Equals,
+    NotEquals,
+    And,
+    Or,
+    Qmark,
+    Defined,
+    LParen,
+    RParen,
+}
+
+impl PreOperator {
+    fn from_op(op: &Operator) -> Self {
+        match op {
+            Operator::Not => PreOperator::Not,
+            Operator::Tilde => PreOperator::Tilde,
+            Operator::Star => PreOperator::Star,
+            Operator::Slash => PreOperator::Slash,
+            Operator::Percent => PreOperator::Percent,
+            Operator::Minus => PreOperator::Minus,
+            Operator::Plus => PreOperator::Plus,
+            Operator::Shl => PreOperator::Shl,
+            Operator::Shr => PreOperator::Shr,
+            Operator::Ushr => PreOperator::Ushr,
+            Operator::Ampersand => PreOperator::Ampersand,
+            Operator::Bitxor => PreOperator::Bitxor,
+            Operator::Bitor => PreOperator::Bitor,
+            Operator::Lt => PreOperator::Lt,
+            Operator::Le => PreOperator::Le,
+            Operator::Gt => PreOperator::Gt,
+            Operator::Ge => PreOperator::Ge,
+            Operator::Equals => PreOperator::Equals,
+            Operator::NotEquals => PreOperator::NotEquals,
+            Operator::And => PreOperator::And,
+            Operator::Or => PreOperator::Or,
+            _ => todo!("Operator: {:?}", op),
+        }
+    }
+
+    fn is_unary(&self) -> bool {
+        matches!(
+            self,
+            PreOperator::Not
+                | PreOperator::Tilde
+                | PreOperator::Negate
+                | PreOperator::Confirm
+                | PreOperator::Defined
+        )
+    }
+
+    fn priority(&self) -> i32 {
+        match self {
+            PreOperator::Not
+            | PreOperator::Tilde
+            | PreOperator::Negate
+            | PreOperator::Confirm
+            | PreOperator::Defined => 2,
+            PreOperator::Star | PreOperator::Slash | PreOperator::Percent => 3,
+            PreOperator::Minus | PreOperator::Plus => 4,
+            PreOperator::Shl | PreOperator::Shr | PreOperator::Ushr => 5,
+            PreOperator::Ampersand => 6,
+            PreOperator::Bitxor => 7,
+            PreOperator::Bitor => 8,
+            PreOperator::Lt | PreOperator::Le | PreOperator::Gt | PreOperator::Ge => 9,
+            PreOperator::Equals | PreOperator::NotEquals => 10,
+            PreOperator::And => 11,
+            PreOperator::Or => 12,
+            PreOperator::Qmark => 13,
+            PreOperator::LParen | PreOperator::RParen => panic!("Invalid operator: {:?}", &self),
+        }
+    }
+}
