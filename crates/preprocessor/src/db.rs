c226cbcd1e9d9f9da925c6e50190ccaf14aa938c
Sarrus1 <omegasix0611@gmail.com>
2024-02-13 10:47:41 +0100

    chore: stdx refactor

--- crates/preprocessor/src/db.rs
+++ crates/preprocessor/src/db.rs
@@ -1,10 +1,8 @@
-use std::{
-    hash::{Hash, Hasher},
-    sync::Arc,
-};
+use std::sync::Arc;
 
 use base_db::{infer_include_ext, SourceDatabase};
-use fxhash::{FxHashMap, FxHashSet};
+use fxhash::FxHashMap;
+use stdx::hashable_hash_map::{HashableHashMap, HashableHashSet};
 use vfs::{AnchoredPath, FileId};
 
 use crate::{Macro, PreprocessingResult, SourcepawnPreprocessor};
@@ -21,48 +19,6 @@ pub trait PreprocDatabase: SourceDatabase {
 
     #[salsa::invoke(preprocess_file_query)]
     fn preprocess_file(&self, file_id: FileId) -> Arc<PreprocessingResult>;
-}
-
-// FIXME: Hopefully there is a way to avoid Hashable Hashmaps?
-#[derive(Debug, Clone, PartialEq, Eq, Default)]
-pub struct HashableHashMap<K: Hash + Eq, V: Eq> {
-    pub map: FxHashMap<K, V>,
-}
-
-impl<K: Hash + Ord, V: Hash + Eq> Hash for HashableHashMap<K, V> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        // Collect and sort the keys to ensure consistent order
-        let mut pairs: Vec<_> = self.map.iter().collect();
-        pairs.sort_by(|a, b| a.0.cmp(b.0));
-
-        // Hash each key-value pair
-        for (key, value) in pairs {
-            key.hash(state);
-            value.hash(state);
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct HashableHashSet<K: Hash + Eq> {
-    pub set: FxHashSet<K>,
-}
-
-impl<K: Hash + Ord> Hash for HashableHashSet<K> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        // Collect and sort the keys to ensure consistent order
-        let mut pairs: Vec<_> = self.set.iter().collect();
-        pairs.sort();
-        pairs.iter().for_each(|k| k.hash(state));
-    }
-}
-
-impl<K: Hash + Eq> Default for HashableHashSet<K> {
-    fn default() -> Self {
-        Self {
-            set: FxHashSet::default(),
-        }
-    }
 }
 
 pub(crate) fn preprocess_file_query(
--- crates/stdx/src/lib.rs
+++ crates/stdx/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod anymap;
+pub mod hashable_hash_map;
 pub mod macros;
 pub mod panic_context;
 pub mod thread;
