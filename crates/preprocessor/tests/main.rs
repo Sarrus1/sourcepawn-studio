83546bd2f5edb0debf034bd73f4f0559b40a1949
Sarrus1 <omegasix0611@gmail.com>
2024-03-10 18:59:34 +0100

    chore: switch to insta fixtures in preprocessor tests

--- crates/preprocessor/tests/main.rs
+++ crates/preprocessor/tests/main.rs
@@ -1,5 +1,7 @@
 use vfs::FileId;
 
+use insta::assert_snapshot;
+
 fn extend_macros(
     _macro_store: &mut MacrosMap,
     mut _path: String,
@@ -11,13 +13,12 @@ fn extend_macros(
 
 #[allow(unused_macros)]
 macro_rules! assert_preproc_eq {
-    ($input:expr, $output:expr) => {
-        assert_eq!(
+    ($input:expr) => {
+        assert_snapshot!(
             SourcepawnPreprocessor::new(FileId::from(0), $input, &mut extend_macros)
                 .preprocess_input()
                 .preprocessed_text()
-                .as_ref(),
-            $output
+                .as_ref()
         );
     };
 }
@@ -30,7 +31,7 @@ fn no_preprocessor_directives() {
         int bar;
         "#;
 
-    assert_preproc_eq!(input, input);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -39,12 +40,8 @@ fn if_directive_defined() {
 #if defined FOO
     int foo;
 #endif"#;
-    let output = r#"#define FOO
 
-    int foo;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -54,13 +51,8 @@ fn if_directive_defined_complex_1() {
     int foo;
     int bar;
 #endif"#;
-    let output = r#"#define FOO
 
-
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -71,14 +63,8 @@ fn if_directive_defined_complex_2() {
     int foo;
     int bar;
 #endif"#;
-    let output = r#"#define FOO
-#define BAR
 
-    int foo;
-    int bar;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -91,16 +77,8 @@ fn if_directive_defined_complex_3() {
     int bar;
     #endif
 #endif"#;
-    let output = r#"#define FOO
-#define BAR
 
-    int foo;
-
-    int bar;
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -114,17 +92,8 @@ fn if_directive_defined_complex_4() {
     int baz;
     #endif
 #endif"#;
-    let output = r#"#define FOO
 
-    int foo;
-
-
-
-    int baz;
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -135,14 +104,8 @@ int foo;
 #else
 int bar;
 #endif"#;
-    let output = r#"#define FOO
 
-int foo;
-
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -153,14 +116,8 @@ int foo;
 #else
 int bar;
 #endif"#;
-    let output = r#"#define FOO
 
-
-
-int bar;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -178,21 +135,8 @@ int bar;
 {
     int bar;
 }"#;
-    let output = r#"#define FOO
 
-public void OnPluginStart()
-
-
-
-
-
-
-
-{
-    int bar;
-}"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -203,22 +147,15 @@ int foo;
 #elseif FOO == 1
 int bar;
 #endif"#;
-    let output = r#"#define FOO 1
 
-int foo;
-
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn define_1() {
     let input = r#"#define FOO 1"#;
-    let output = r#"#define FOO 1"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -226,11 +163,8 @@ fn define_trailing_comment_1() {
     let input = r#"#define FOO 1 /* comment */
 int foo = 1;
 "#;
-    let output = r#"#define FOO 1 /* comment */
-int foo = 1;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -242,15 +176,8 @@ int foo;
 
 #endif
 "#;
-    let output = r#"#define FOO bar /**< documentation */
-#include "file.sp"
 
-
-
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -259,12 +186,8 @@ fn if_directive_expansion_1() {
 #if FOO
     int foo;
 #endif"#;
-    let output = r#"#define FOO 1
 
-    int foo;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -273,12 +196,8 @@ fn if_directive_expansion_2() {
 #if FOO == 1
     int foo;
 #endif"#;
-    let output = r#"#define FOO 1
 
-    int foo;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -288,13 +207,8 @@ fn if_directive_expansion_3() {
 #if FOO(1, BAR(2, 3 + 4)) == 10
 #endif
 "#;
-    let output = r#"#define FOO(%0,%1) %0 + %1
-#define BAR(%0,%1) %0 + %1
 
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -304,13 +218,8 @@ fn if_directive_nested_expansion_1() {
 #if FOO == 1
     int foo;
 #endif"#;
-    let output = r#"#define FOO BAR
-#define BAR 1
 
-    int foo;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -321,14 +230,8 @@ fn if_directive_nested_expansion_2() {
 #if FOO == 10
     int foo;
 #endif"#;
-    let output = r#"#define FOO BAR + 4
-#define BAR 1 + BAZ
-#define BAZ 2 + 3
 
-    int foo;
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -338,13 +241,8 @@ fn if_directive_nested_expansion_infinite_loop_1() {
 #if FOO == 1
     int foo;
 #endif"#;
-    let output = r#"#define FOO BAR
-#define BAR FOO
 
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -356,15 +254,8 @@ int foo;
 int bar;
 #endif
 "#;
-    let output = r#"#define FOO 1
 
-
-
-int bar;
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -376,15 +267,8 @@ int foo;
 int bar;
 #endif
 "#;
-    let output = r#"#define FOO 1
 
-int foo;
-
-
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -398,17 +282,8 @@ fn elseif_directive_expansion_3() {
 int bar;
 #endif
 "#;
-    let output = r#"#define FOO 1
 
-
-    int foo;
-
-
-
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -422,17 +297,8 @@ int bar;
 int baz;
 #endif
 "#;
-    let output = r#"#define FOO 3
 
-
-
-
-
-int baz;
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -448,19 +314,8 @@ int bar;
 int baz;
 #endif
 "#;
-    let output = r#"#define FOO 3
 
-
-
-
-
-
-
-int baz;
-
-"#;
-
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -468,11 +323,8 @@ fn define_undef_1() {
     let input = r#"#define FOO 1
 #undef FOO
 "#;
-    let output = r#"#define FOO 1
-#undef FOO
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -480,11 +332,8 @@ fn define_expansion_1() {
     let input = r#"#define FOO 1
 int foo = FOO;
 "#;
-    let output = r#"#define FOO 1
-int foo = 1;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -492,11 +341,8 @@ fn define_expansion_2() {
     let input = r#"#define FOO "test"
 char foo[64] = FOO;
 "#;
-    let output = r#"#define FOO "test"
-char foo[64] = "test";
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -504,11 +350,8 @@ fn define_expansion_comment_1() {
     let input = r#"#define FOO 1 //comment
 int foo = FOO;
 "#;
-    let output = r#"#define FOO 1 //comment
-int foo = 1;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -516,11 +359,8 @@ fn define_expansion_comment_2() {
     let input = r#"#define FOO 1 /* comment */
 int foo = FOO;
 "#;
-    let output = r#"#define FOO 1 /* comment */
-int foo = 1;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -529,12 +369,8 @@ fn define_expansion_comment_3() {
 comment */
 int foo = FOO;
 "#;
-    let output = r#"#define FOO 1 /* long\
-comment */
-int foo = 1;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -544,13 +380,8 @@ comment */ + 2
 int foo = FOO;
 int bar;
 "#;
-    let output = r#"#define FOO 1 /* long\
-comment */ + 2
-int foo = 1 + 2;
-int bar;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -560,13 +391,8 @@ comment */ + 2 // Line comment
 int foo = FOO;
 int bar;
 "#;
-    let output = r#"#define FOO 1 /* long\
-comment */ + 2 // Line comment
-int foo = 1 + 2;
-int bar;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -575,12 +401,8 @@ fn define_expansion_line_continuation_1() {
 expansion"
 char foo[64] = FOO;
 "#;
-    let output = r#"#define FOO "test \
-expansion"
-char foo[64] = "test expansion";
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -591,14 +413,8 @@ expansion \
 also"
 char foo[64] = FOO;
 "#;
-    let output = r#"#define FOO "test \
-expansion \
-\
-also"
-char foo[64] = "test expansion also";
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -607,12 +423,8 @@ fn define_expansion_nested_1() {
 #define BAR 1
 int foo = FOO;
 "#;
-    let output = r#"#define FOO BAR +   2
-#define BAR 1
-int foo = 1 +   2;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -622,13 +434,8 @@ fn define_expansion_nested_2() {
 #define BAZ 2
 int foo = FOO;
 "#;
-    let output = r#"#define FOO BAR + 3
-#define BAR 1 + BAZ
-#define BAZ 2
-int foo = 1 + 2 + 3;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -637,12 +444,8 @@ fn define_expansion_nested_3() {
 #define BAR 1 + 2
 int foo = FOO;
 "#;
-    let output = r#"#define FOO BAR + 3
-#define BAR 1 + 2
-int foo = 1 + 2 + 3;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -651,12 +454,8 @@ fn define_expansion_nested_4() {
 #define BAR FOO + 3 + 4
 int bar = BAR;
 "#;
-    let output = r#"#define FOO 1 + 2
-#define BAR FOO + 3 + 4
-int bar = 1 + 2 + 3 + 4;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -664,11 +463,8 @@ fn macro_expansion_1() {
     let input = r#"#define FOO(%0,%1) %0 + %1
 int foo = FOO(1, 2);
 "#;
-    let output = r#"#define FOO(%0,%1) %0 + %1
-int foo = 1 + 2;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -676,11 +472,8 @@ fn macro_expansion_2() {
     let input = r#"#define FOO(%0) %0 %%2
 int foo = FOO(2);
 "#;
-    let output = r#"#define FOO(%0) %0 %%2
-int foo = 2 %2;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -689,12 +482,8 @@ fn macro_expansion_3() {
 #define BAR(%0,%1) %0 + %1
 int foo = FOO(1, BAR(2, 3 + 4));
 "#;
-    let output = r#"#define FOO(%0,%1) %0 + %1
-#define BAR(%0,%1) %0 + %1
-int foo = 1 + 2 + 3 + 4;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -702,11 +491,8 @@ fn macro_expansion_4() {
     let input = r#"#define FOO(%1,%2) %1 + %2
 int foo = FOO(1, 2);
 "#;
-    let output = r#"#define FOO(%1,%2) %1 + %2
-int foo = 1 + 2;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -714,11 +500,8 @@ fn macro_expansion_5() {
     let input = r#"#define FOO(%1) int %1
 FOO(foo, bar);
 "#;
-    let output = r#"#define FOO(%1) int %1
-int foo, bar;
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -730,15 +513,8 @@ fn macro_expansion_6() {
         return i;}
         
         GET_VALUE(void, Foo)"#;
-    let output = r#"#define GET_VALUE(%1,%2) \
-    public %1 Get%2(){ \
-        %1 i; \
-        this.GetValue("m_" ... #%2, i); \
-        return i;}
-        
-    public void GetFoo(){        void i;        this.GetValue(        "m_" ...        "Foo", i);        return i;}"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -747,12 +523,8 @@ fn macro_expansion_7() {
 #define BAR(%0,%1) 1 + FOO(%0, %1)
 int foo = BAR(2, 3)
 "#;
-    let output = r#"#define FOO(%0,%1) %0 + %1
-#define BAR(%0,%1) 1 + FOO(%0, %1)
-int foo = 1 + 2 + 3
-"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -763,12 +535,7 @@ public void OnPluginStart() {
 }
 "#;
 
-    let output = r#"#define FOO(%1) #%1
-public void OnPluginStart() {
-    PrintToServer("foo");
-}
-"#;
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -779,7 +546,7 @@ public void OnPluginStart() {
         (foo));
 }
 "#;
-    assert_preproc_eq!(input, input);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -789,7 +556,7 @@ public void OnPluginStart() {
     PrintToServer(FOO);
 }
 "#;
-    assert_preproc_eq!(input, input);
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -801,66 +568,54 @@ public void OnPluginStart() {
 }
 "#;
 
-    let output = r#"#define FOO(%1) %1
-#define BAR(%1) %1
-public void OnPluginStart() {
-    PrintToServer(FOO);
-}
-"#;
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn include_directive_1() {
     let input = r#"#include <sourcemod>"#;
-    let output = r#"#include <sourcemod>"#;
 
-    assert_preproc_eq!(input, output);
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn include_directive_2() {
     let input = r#"#include <sourcemod\
 >"#;
-    let output = r#"#include <sourcemod>
-"#;
-    assert_preproc_eq!(input, output);
+
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn include_directive_3() {
     let input = r#"#include <sourcemod>
 "#;
-    let output = r#"#include <sourcemod>
-"#;
-    assert_preproc_eq!(input, output);
+
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn stringizing_1() {
     let input = r#"#define FOO(%0) #%0
 char foo[8] = FOO(foo);"#;
-    let output = r#"#define FOO(%0) #%0
-char foo[8] = "foo";"#;
-    assert_preproc_eq!(input, output);
+
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn stringizing_2() {
     let input = r#"#define FOO(%0,%1) #%0 ... #%1
 char foo[8] = FOO(foo, bar);"#;
-    let output = r#"#define FOO(%0,%1) #%0 ... #%1
-char foo[8] = "foo" ... "bar";"#;
-    assert_preproc_eq!(input, output);
+
+    assert_preproc_eq!(input);
 }
 
 #[test]
 fn stringizing_3() {
     let input = r#"#define FOO(%0) #%0
 char foo[8] = FOO(foo , bar);"#;
-    let output = r#"#define FOO(%0) #%0
-char foo[8] = "foo , bar";"#;
-    assert_preproc_eq!(input, output);
+
+    assert_preproc_eq!(input);
 }
 
 #[test]
@@ -872,12 +627,6 @@ fn stringizing_4() {
 void foo(){
     DISPOSE_MEMBER(Foo)
 }"#;
-    let output = r#"#define DISPOSE_MEMBER(%1) \
-    Handle m_h%1; \
-    if(this.GetValue("m_" ... #%1, m_h%1)){ \
-        delete m_h%1;}
-void foo(){
-    Handle m_hFoo;    if(this.GetValue(    "m_" ...    "Foo", m_hFoo)){        delete m_hFoo;}
-}"#;
-    assert_preproc_eq!(input, output);
+
+    assert_preproc_eq!(input);
 }
