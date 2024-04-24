8b6576a533013562eaca07a73a73d3cdfb77c343
Sarrus1 <omegasix0611@gmail.com>
2024-04-23 19:33:35 -0700

    fix: crypto.node not found

--- editors/code/esbuild.mjs
+++ editors/code/esbuild.mjs
@@ -16,42 +16,6 @@ const wasmPlugin = {
   },
 };
 
-const nativeNodeModulesPlugin = {
-  name: "native-node-modules",
-  setup(build) {
-    // If a ".node" file is imported within a module in the "file" namespace, resolve
-    // it to an absolute path and put it into the "node-file" virtual namespace.
-    build.onResolve({ filter: /\.node$/, namespace: "file" }, (args) => ({
-      path: require.resolve(args.path, { paths: [args.resolveDir] }),
-      namespace: "node-file",
-    }));
-
-    // Files in the "node-file" virtual namespace call "require()" on the
-    // path from esbuild of the ".node" file in the output directory.
-    build.onLoad({ filter: /.*/, namespace: "node-file" }, (args) => ({
-      contents: `
-        import path from ${JSON.stringify(args.path)}
-        try { module.exports = require(path) }
-        catch {}
-      `,
-    }));
-
-    // If a ".node" file is imported within a module in the "node-file" namespace, put
-    // it in the "file" namespace where esbuild's default loading behavior will handle
-    // it. It is already an absolute path since we resolved it to one above.
-    build.onResolve({ filter: /\.node$/, namespace: "node-file" }, (args) => ({
-      path: args.path,
-      namespace: "file",
-    }));
-
-    // Tell esbuild's default loading behavior to use the "file" loader for
-    // these ".node" files.
-    let opts = build.initialOptions;
-    opts.loader = opts.loader || {};
-    opts.loader[".node"] = "file";
-  },
-};
-
 if (!fs.existsSync(outDir)) {
   fs.mkdirSync(outDir);
 }
@@ -68,8 +32,8 @@ let ctx = await esbuild.build({
   external: ["vscode"],
   format: "cjs",
   platform: "node",
-  loader: { ".node": "copy" },
-  plugins: [wasmPlugin, nativeNodeModulesPlugin],
+  loader: { ".node": "file" },
+  plugins: [wasmPlugin],
 });
 
 if (watch) {
