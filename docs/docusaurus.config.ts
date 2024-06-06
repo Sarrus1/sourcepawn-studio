ff591d87a6ba8be1854c207d6b38e82ad83573d5
Sarrus1 <omegasix0611@gmail.com>
2024-06-06 00:00:10 -0700

    add social card

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/sp-studio-social-card_500.png",
     navbar: {
       title: "SourcePawn Studio",
       logo: {
