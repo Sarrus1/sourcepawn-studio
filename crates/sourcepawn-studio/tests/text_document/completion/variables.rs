69d3fdfad2839fe157c1af027dc8b9b7f12ec275
Sarrus1 <omegasix0611@gmail.com>
2025-06-04 23:46:51 +0100

    add variables tests

--- crates/sourcepawn-studio/tests/text_document/completion/variables.rs
+++ crates/sourcepawn-studio/tests/text_document/completion/variables.rs
@@ -16,6 +16,66 @@ int foo;
 }
 
 #[test]
+fn global_variable_2() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+int foo = 1;
+
+|
+^"#,
+        None
+    ));
+}
+
+#[test]
+fn global_variable_3() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+int foo[16] = {1, ...};
+
+|
+^"#,
+        None
+    ));
+}
+
+#[test]
+fn global_variable_in_function() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+int foo;
+
+void main() {
+
+|
+^
+}"#,
+        None
+    ));
+}
+
+#[test]
+fn global_variable_in_function_block() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+int foo;
+
+void main() {
+  for(;;) {
+    
+    |
+    ^
+  }
+}"#,
+        None
+    ));
+}
+
+#[test]
 fn global_variable_include_1() {
     assert_json_snapshot!(complete(
         r#"
@@ -43,6 +103,85 @@ int bar;
 
 |
 ^"#,
+        None
+    ));
+}
+
+#[test]
+fn local_variable_in_function() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+void main() {
+  int foo;
+  
+  |
+  ^
+}"#,
+        None
+    ));
+}
+
+#[test]
+fn local_variable_outside_function() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+void main() {
+  int foo;
+}
+
+|
+^
+"#,
+        None
+    ));
+}
+
+#[test]
+fn local_variable_in_other_function() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+void main() {
+  int foo;
+}
+
+void bar() {
+
+|
+^
+}"#,
+        None
+    ));
+}
+
+#[test]
+fn function_parameter() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+void main(int foo) {
+
+|
+^
+}"#,
+        None
+    ));
+}
+
+#[test]
+fn local_variable_for_loop() {
+    assert_json_snapshot!(complete(
+        r#"
+%! main.sp
+void main() {
+  for(int foo;;) {
+    
+    |
+    ^
+  }
+}"#,
         None
     ));
 }
